97982ddd6eb00740904248aba832898c722025e3
Abraham Moreno <adiaz2821@gmail.com>
2019-10-19 23:39:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end 
 
+<<<<<<< HEAD
 def batch_badge_creator(names)
   names.map do |name|
    badge_maker(name)
@@ -24,3 +25,12 @@ def printer(attendees)
   end 
   
 end 
+=======
+def batch_badge_creator(attendees)
+  badges = []
+attendees.each do |badge|
+  attendees.push(badge_maker(name))
+end
+
+end
+>>>>>>> 6812c38245af60236038b1dd4bbda5a226f4809d
